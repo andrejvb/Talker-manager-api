9a0ac9ebb2bc15dc39305cbd775decd4938cbf71
andrejvb <andre.jvb@gmail.com>
2023-04-17 16:37:17 -0300

    second requirement

--- src/index.js
+++ src/index.js
@@ -17,3 +17,4 @@ app.listen(PORT, () => {
 });
 
 app.use('/talker', talkerRoutes);
+// app.use('/talker:id', talkerRoutes);
--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -15,4 +15,18 @@ router.get('/', async (_req, res) => {
   }  
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+      const idReq = Number(req.params.id);  
+      const dataTalkers = await readFile();
+      const chosenTalker = dataTalkers.find((talker) => talker.id === idReq);
+      if (!chosenTalker) {
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });          
+      }
+      return res.status(200).json(chosenTalker);
+    } catch (error) {
+      return null;
+    }  
+  });
+
 module.exports = router;
